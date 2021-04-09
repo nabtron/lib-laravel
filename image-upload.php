a875addd76db213bd2fefd38c5ccbea14f0cf20e
nabtron <developer@nabtron.com>
2021-04-09 19:26:35 +0500

    Create image-upload.php

---
+++ image-upload.php
@@ -0,0 +1,25 @@
+    public function save(Request $request) {
+
+        $month = date('m');
+
+        if($request->hasfile('imageFile')) {
+            
+            $request->validate([
+                'imageFile' => 'nullable',
+                'imageFile.*' => 'mimes:jpeg,jpg,png,gif|max:2048'
+            ]);
+        
+            foreach($request->imageFile as $file)
+            {
+                $name = time() . $file->getClientOriginalName();
+                $file->move(public_path('uploads/' . $month . '/'), $name);
+                $imgData[] = $name;  
+            }
+    
+            // return "succsess";
+            // return back()->with('success', 'File has successfully uploaded!');
+
+        }
+    }
+
+// submit the image from form with name as array imageFile[]
